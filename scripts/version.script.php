0a94aec8c8c113688ae4a246e194e88e5d13740c
Andrew Male <mamale@outlook.com>
2025-02-06 02:16:38 -0500

    Adding scripts folder

---
+++ scripts/version.script.php
@@ -0,0 +1,62 @@
+<?php
+
+	namespace Zsf\Scripts;
+
+	use AndyM84\Config\ConfigContainer;
+
+	use Stoic\Utilities\ConsoleHelper;
+	use Stoic\Utilities\FileHelper;
+
+	use Zsf\Utils\ZsfCliScript;
+
+	class VersionScript implements ZsfCliScript {
+		public function key() : string {
+			return 'version';
+		}
+
+		public function run(ConsoleHelper $ch, FileHelper $fh, ConfigContainer $config) : void {
+			$ch->putLine('ZSF Version Information');
+			$ch->putLine('-----------------------');
+			$ch->putLine();
+
+			if (defined('ZSF_VERSION')) {
+				$ch->putLine('Current ZSF version: ' . ZSF_VERSION);
+			} else {
+				$ch->putLine('Current ZSF version: Unknown');
+			}
+
+			if ($config->has('systemVersion')) {
+				$ch->putLine('System version:      ' . $config->get('systemVersion'));
+			} else {
+				$ch->putLine('System version:      Unknown');
+			}
+
+			$ch->putLine();
+
+			if ($fh->fileExists('~/vendor/composer/installed.json')) {
+				$installed = json_decode($fh->getContents('~/vendor/composer/installed.json'), true);
+
+				if (is_array($installed)) {
+					$ch->putLine('Installed composer packages:');
+
+					$longest = 0;
+					$pairs   = [];
+
+					foreach ($installed['packages'] as $package) {
+						$pairs[$package['name']] = $package['version'];
+						$longest                 = max($longest, strlen($package['name']));
+					}
+
+					foreach ($pairs as $name => $version) {
+						$ch->putLine('    ' . str_pad($name, $longest) . '  ' . $version);
+					}
+				}
+			} else {
+				$ch->putLine('Installed composer packages: Unknown / None found');
+			}
+
+			$ch->putLine();
+
+			return;
+		}
+	}
