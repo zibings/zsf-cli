d60893240ff2081861050fd34493d58fcb6b4262
Andrew Male <mamale@outlook.com>
2025-02-06 01:02:33 -0500

    Adding code doc to Zsf\Utils\isRunningFile()

--- Zsf/Utils/CliFunctions.php
+++ Zsf/Utils/CliFunctions.php
@@ -2,6 +2,12 @@
 
 	namespace Zsf\Utils;
 
+	/**
+	 * Determines whether the given filename is being called directly from the command line.
+	 *
+	 * @param string $fileName
+	 * @return bool
+	 */
 	function isRunningFile(string $fileName) : bool {
 		return basename($fileName) === basename($_SERVER['SCRIPT_FILENAME']);
 	}
