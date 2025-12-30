ca94e4af176e78c986d662389f40ac2db0ac8e9b
Andrew Male <mamale@outlook.com>
2025-12-30 03:40:07 -0500

    Adding JS translator

---
+++ Zsf/Utils/Translators/JavaScriptTranslator.php
@@ -0,0 +1,29 @@
+<?php
+
+	namespace Zsf\Utils\Translators;
+
+	class JavaScriptTranslator implements ITranslator {
+		public function toPhpType(string $type) : string {
+			return match ($type) {
+				'number'  => 'float|int',
+				'string'  => 'string',
+				'boolean' => 'bool',
+				'Array'   => 'array',
+				'object'  => 'object',
+				'null'    => 'null',
+				default   => 'mixed',
+			};
+		}
+
+		public function toTranslatedType(string $type) : string {
+			return match ($type) {
+				'float', 'int' => 'number',
+				'string'       => 'string',
+				'bool'         => 'boolean',
+				'array'        => 'Array',
+				'object'       => 'object',
+				'null'         => 'null',
+				default        => 'any',
+			};
+		}
+	}
