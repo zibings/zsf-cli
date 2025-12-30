5d3d987435f54e3bdee3ce16ac6c0c89f5f5ce60
Andrew Male <mamale@outlook.com>
2025-12-30 03:41:08 -0500

    Adding SQLServer translator

---
+++ Zsf/Utils/Translators/SQLServerTranslator.php
@@ -0,0 +1,25 @@
+<?php
+
+	namespace Zsf\Utils\Translators;
+
+	class SQLServerTranslator implements ITranslator {
+		public function toPhpType(string $type) : string {
+			return match ($type) {
+				'int', 'bigint', 'smallint', 'tinyint'                   => 'int',
+				'float', 'real', 'decimal', 'numeric'                    => 'float',
+				'bit'                                                    => 'bool',
+				'date', 'datetime', 'datetime2', 'smalldatetime', 'time' => '\DateTimeInterface',
+				default                                                  => 'string',
+			};
+		}
+
+		public function toTranslatedType(string $type) : string {
+			return match ($type) {
+				'int'                => 'integer',
+				'float'              => 'decimal',
+				'bool'               => 'bit',
+				'\DateTimeInterface' => 'datetime',
+				default              => 'string',
+			};
+		}
+	}
